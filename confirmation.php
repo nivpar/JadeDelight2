4d10213aebad0298b2f4df2cae9dda40adccbf6d
nivpar <71618752+nivpar@users.noreply.github.com>
2020-11-15 14:35:12 -0500

    Add files via upload

---
+++ confirmation.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Customer's Order</title>
+</head>
+
+<body>
+	<?php echo $_POST["quan1"]; ?>!<br>
+	<?php echo $_POST["quan2"]; ?>!<br>
+	<?php echo $_POST["quan3"]; ?>!<br>
+	<?php echo $_POST["quan4"]; ?>!<br>
+	<?php echo $_POST["quan5"]; ?>!<br>
+</body>
+</html>
